910ec34d30426e9aa0f9089dd3857a4b00b0102f
xiaochunqi135 <97508950+xiaochunqi135@users.noreply.github.com>
2023-01-22 23:43:13 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -92,11 +92,11 @@ mod tests {
 #[cfg(test)]
 pub mod pg_test {
     pub fn setup(_options: Vec<&str>) {
-        // perform one-off initialization when the pg_test framework starts
+        // perform one-off initialization when the pg_test framework starts.
     }
 
     pub fn postgresql_conf_options() -> Vec<&'static str> {
-        // return any postgresql.conf settings that are required for your tests
+        // return any postgresql.conf settings that are required for your tests.
         vec![]
     }
 }
